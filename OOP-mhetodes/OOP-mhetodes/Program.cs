87958ca882b8b9920a4a66ad9c3a0905564baabb
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-17 08:35:22 +0200

    .

---
+++ OOP-mhetodes/OOP-mhetodes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_mhetodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
