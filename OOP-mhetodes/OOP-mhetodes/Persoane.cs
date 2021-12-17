062892d916ecfc82ae260a696a4ca63846c5299c
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-17 08:50:24 +0200

    .

---
+++ OOP-mhetodes/OOP-mhetodes/Persoane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_mhetodes
+{
+    public class Persoane
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+
+        }
+        public void SayHello(string introductionFormula)
+        {
+            Console.Write($"{introductionFormula} {FullName}");
+        }
+        public int GetAge()
+        {
+            return DateTime.Today.Year - DateOfBirth.Year;
+        }
+    }
+}
